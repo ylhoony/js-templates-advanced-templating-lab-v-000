e2a2af390e01718abdc8dd0e369f5b66d37ec35d
Hoon Lee <yl.hoony@gmail.com>
2018-04-12 22:21:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ function createRecipe() {
   document.getElementById("main").innerHTML = template(recipe)
 }
 
+// function updateRecipe() {
+//   var recipe = getRecipeVals()
+//   var recipeTemplate = document.getElementById("recipe-template").innerHTML
+//   var template = Handlebars.compile(recipeTemplate)
+//   document.getElementById("main").innerHTML = template(recipe)
+// }
+
 function displayEditForm() {
   var name = document.getElementById("nameHeader").innerText
   var description = document.getElementById("recipeDescription").innerText
